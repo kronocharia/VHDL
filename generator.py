aebd2523df93f2df008cede92ce9bad052cdc837
kronocharia <mail@mike-li.co.uk>
2014-03-21 15:28:33 +0000

    [RCB] Python generator

---
+++ generator.py
@@ -0,0 +1,17 @@
+import random
+
+numCommands = 20
+with open("randomcommands.txt", "w+") as fo:
+
+	for i in range(numCommands):
+
+		rand_command = random.choice(['D','C'])
+		rand_colour = random.choice(['W','B','I'])
+		rand_xCoor = random.randint(0,2**6)
+		rand_yCoor = random.randint(0,2**6)
+
+
+		# print rand_command+rand_colour, rand_xCoor, rand_yCoor
+		fo.write(rand_command+rand_colour+" "+str(rand_xCoor)+" "+str(rand_yCoor)+'\n')
+
+print "test file created"
