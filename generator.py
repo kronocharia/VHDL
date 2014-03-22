668c7533b84d617f85722da2590ccaa70850d580
kronocharia <mail@mike-li.co.uk>
2014-03-22 21:42:32 +0000

    ADD generator

--- generator.py
+++ generator.py
@@ -1,15 +1,18 @@
 import random
 
-numCommands = 20
+numCommands = 50000
+pen = (0, 0)
 with open("randomcommands.txt", "w+") as fo:
-
 	for i in range(numCommands):
-
-		rand_command = random.choice(['D','C'])
+		rand_command = random.choice(['D','C','M'])
 		rand_colour = random.choice(['W','B','I'])
-		rand_xCoor = random.randint(0,2**6)
-		rand_yCoor = random.randint(0,2**6)
-
+		if (rand_command == 'C'):
+			rand_xCoor = random.randint(pen[0],63)
+			rand_yCoor = random.randint(pen[1],63)
+		else:
+			rand_xCoor = random.randint(0,63)
+			rand_yCoor = random.randint(0,63)
+		pen = (rand_xCoor, rand_yCoor)
 
 		# print rand_command+rand_colour, rand_xCoor, rand_yCoor
 		fo.write(rand_command+rand_colour+" "+str(rand_xCoor)+" "+str(rand_yCoor)+'\n')
